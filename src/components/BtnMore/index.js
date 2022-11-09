d4395a9a4120b88e1caf384140d8ccf00443e11c
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-09 16:26:39 +0900

    feat: add BtnMore

---
+++ src/components/BtnMore/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './BtnMore.css'
+
+const BtnMore = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [hoverColor, setHoverColor] = useState("#303030")
+
+    return (
+        <button className='c-btn__container'
+            onMouseEnter={() => { setHoverColor("#999999") }}
+            onMouseLeave={() => { setHoverColor("#303030") }}
+            onClick={() => { setIsActive(!isActive) }}>
+            <span className='c-btn__text'>{!isActive ? "MORE" : "LESS"}</span>
+            <div className='c-btn__arrow__container'>
+                {!isActive ?
+                    <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill={hoverColor} cx="20" cy="20" r="20" /><path stroke="#FFF" strokeWidth="2" fill="none" d="m14 21 6 6 6-6" /></g></svg> :
+                    <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill={hoverColor} cx="20" cy="20" r="20" /><path stroke="#FFF" strokeWidth="2" d="M14 23l6-6 6 6" /></g></svg>
+                }
+            </div>
+        </button>
+    )
+}
+
+export default BtnMore
