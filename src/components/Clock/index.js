b93c68798ffaf962b21f638072ca43fb6535735c
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-10 19:07:43 +0900

    feat(clock): add realtime clock update

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,37 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import moment from 'moment/moment';
 
 const Clock = () => {
-
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [dataClock, setDataClock] = useState([]);
 
-    //This deps empty array means that the useEffec will run just once
+    
+    const [time, setTime] = useState(new Date().toLocaleTimeString())
+    function clock(){
+        const newTime = new Date().toLocaleTimeString();
+        setTime(newTime);
+    }
+    setInterval(clock,1000);
+    
+    
+    // This deps empty array means that the useEffec will run just once
     useEffect(() => {
-        fetch("http://worldtimeapi.org/api/ip")
+            fetch("http://worldtimeapi.org/api/ip")
             .then(data => data.json())
             .then(
                 (result) => {
                     setIsLoaded(true);
-                    setDataClock(JSON.stringify(result));
-                }
-            ,
+                    setDataClock(result);
+                },
                 // Here it's important to handle the errors
                 (error) => {
                     setIsLoaded(true);
                     setError(error);
                 }
-            )
-    }, [])
+                )
+    },[])
 
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-        return <div>Loading...</div>
+        return <div>Updating...</div>
     } else {
         return (
             <div>
-                {dataClock}
+                {time}
+                {console.log(dataClock)}
             </div>
         )
     }
