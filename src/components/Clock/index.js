5e3cf40038957c8f63a1e9cfc9ba8a0e0d30783f
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-13 03:16:50 +0900

    refactor(clock): change responsibility for receiving data to useApi

--- src/components/Clock/index.js
+++ src/components/Clock/index.js
@@ -1,44 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import moment from 'moment/moment';
+import { useState } from 'react';
+import { useApi } from '../../api/useApi/useApi';
 import './Clock.css'
 
 const Clock = () => {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [dataClock, setDataClock] = useState([]);
 
-    const [time, setTime] = useState(new Date)
+    const [dataClock, isLoaded, error] = useApi("http://worldtimeapi.org/api/ip");
 
-    function clock() {
+    const [time, setTime] = useState(new Date())
+
+    function displayClock() {
         const newTime = new Date()
         newTime.setHours(newTime.getUTCHours() + parseInt(dataClock.utc_offset.substr(0, 3)))
         setTime(newTime);
     }
-
-
-    // This deps empty array means that the useEffec will run just once
-    useEffect(() => {
-        fetch("http://worldtimeapi.org/api/ip")
-            .then(data => data.json())
-            .then(
-                (result) => {
-                    setIsLoaded(true);
-                    setDataClock(result);
-                },
-                // Here it's important to handle the errors
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            )
-    }, [])
 
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
         return <div>Updating...</div>
     } else {
-        setInterval(clock, 1000);
+        setInterval(displayClock, 1000);
         const amPm = time.toLocaleTimeString('en-US').substring(8)
         const hours = (time.getHours() % 12 || 12) >= 10 ? (time.getHours() % 12 || 12)  : "0" + (time.getHours() % 12 || 12)
         const minutes = time.getMinutes() >= 10 ? time.getMinutes() : "0" + time.getMinutes()
