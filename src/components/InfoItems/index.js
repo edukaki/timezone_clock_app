d9dab1d618dbd98ef3edb01de1830420d8e7f2b3
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-14 03:31:47 +0900

    feat(infoItems): add div to separate items

--- src/components/InfoItems/index.js
+++ src/components/InfoItems/index.js
@@ -3,10 +3,10 @@ import './InfoItems.css'
 
 const InfoItems = (props) => {
     return (
-        <>
+        <div>
             <h2 className='c-info-items__title'>{props.title}</h2>
             <span className='c-info-items__description'>{props.description}</span>
-        </>
+        </div>
     )
 }
 
