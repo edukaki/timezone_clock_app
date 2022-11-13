e01e6e0d324dee60e9ff1d1c506b893c556fbfe8
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-13 21:15:13 +0900

    style(quotes): import css

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useApi } from '../../api/useApi/useApi'
+import './Quotes.css'
 
 const Quotes = () => {
     const [getQuotes] = useApi("https://programming-quotes-api.herokuapp.com/quotes/random");
     return (
-        <div className="c-title">
+        <div className="c-quotes">
             <p>“{getQuotes.en}”</p>
             <span>{getQuotes.author}</span>
         </div>
