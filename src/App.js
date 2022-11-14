358f67e74c66bd9c42d4ea46d19ce64c31a6be0f
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-11-14 16:29:24 +0900

    feat(ap): add getDaytime function to set the theme to use

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useApi } from "./api/useApi/useApi";
 import BtnMore from "./components/BtnMore";
 import Clock from "./components/Clock";
@@ -9,18 +10,23 @@ function App() {
   const [getQuotes] = useApi("https://programming-quotes-api.herokuapp.com/quotes/random");
   const [location] = useApi("https://api.ipgeolocation.io/ipgeo?apiKey=51765e4a180b43ed9c232935a781ac71")
 
+  const getDayTime = (theme) => {
+    const body = document.body
+    body.classList.add(theme)
+  }
+
   return (
     <main className="l-main">
       <div className="container container--active">
         <div className="l-column">
-          <Quotes quotes={getQuotes}/>
-          <Clock location={location} dataClock={dataClock} isLoaded={isLoaded} error={error} />
+          <Quotes quotes={getQuotes} />
+          <Clock getDayTime={getDayTime} location={location} dataClock={dataClock} isLoaded={isLoaded} error={error} />
         </div>
         <div className="l-column">
           <BtnMore />
         </div>
       </div>
-        <InfoMenu data={dataClock} />
+      <InfoMenu data={dataClock} />
     </main>
   );
 }
